c98dfe7f516dff27c7fea70f59777a4fb8078f70
Sohan Hg <sohanhg12@gmail.com>
2025-05-13 15:22:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Mini-Project/', // Set base URL for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
